6a1c8315a713ea5095e1944cf8c23249f5d4cd3f
Jorrit Salverda <jsalverda@travix.com>
2018-06-05 15:37:43 +0200

    hide columns for smaller screens

--- app/routes/pipeline-build-details.js
+++ app/routes/pipeline-build-details.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-    afterModel(model, transition) {
+    afterModel(model) {
         this.replaceWith('pipeline-build-logs', model.repoSource ,model.repoOwner, model.repoName, model.repoRevision); // Implicitly aborts the on-going transition.
     },
 });
