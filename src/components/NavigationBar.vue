734a38cc226bdd5628535fea5e026763d8a2a9c5
Jorrit Salverda <jorrit.salverda@gmail.com>
2018-06-11 17:45:58 +0200

    load navigationbar

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,14 +1,14 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <router-link :to="{ name: 'Home'}">Estafette CI</router-link>
+    <router-link :to="{ name: 'Home'}" class="navbar-brand">Estafette CI</router-link>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-            <router-link :to="{ name: 'Pipelines'}">Pipelines</router-link>
-            <router-link :to="{ name: 'Targets'}">Targets</router-link>
-            <router-link :to="{ name: 'Statistics'}">Statistics</router-link>
+            <router-link :to="{ name: 'Pipelines'}" class="nav-item nav-link">Pipelines</router-link>
+            <router-link :to="{ name: 'Targets'}" class="nav-item nav-link">Targets</router-link>
+            <router-link :to="{ name: 'Statistics'}" class="nav-item nav-link">Statistics</router-link>
         </div>
     </div>
     </nav>
@@ -16,9 +16,6 @@
 
 <script>
 export default {
-  name: 'navigation-bar',
-  data: function () {
-    return {}
-  }
+  name: 'NavigationBar'
 }
 </script>
