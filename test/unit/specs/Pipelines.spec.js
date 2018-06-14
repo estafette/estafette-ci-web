8e5126ba655f1a839ed768f9a6e4a6b806fdbd60
Jorrit Salverda <jsalverda@travix.com>
2018-06-14 15:35:09 +0200

    fix unit test

--- test/unit/specs/Pipelines.spec.js
+++ test/unit/specs/Pipelines.spec.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-import PipelinesList from '@/components/PipelinesList'
+import Pipelines from '@/components/Pipelines'
 
-describe('PipelinesList.vue', () => {
+describe('Pipelines.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(PipelinesList)
+    const Constructor = Vue.extend(Pipelines)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('table th').textContent)
       .toEqual('Pipeline')
