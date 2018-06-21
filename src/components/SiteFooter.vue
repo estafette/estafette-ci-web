f37ef1274f31b590f757b7694cb991bba4023f52
Jorrit Salverda <jsalverda@travix.com>
2018-06-21 17:23:44 +0200

    add link to file an issue

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -2,5 +2,6 @@
     <nav class="nav justify-content-end bg-dark p-2">
         <a href="https://estafette.io/documentation/" target="_blank" class="nav-link text-light">Documentation</a>
         <a href="https://github.com/estafette/estafette-ci" target="_blank" class="nav-link text-light">Github</a>
+        <a href="https://github.com/estafette/estafette-ci/issues/new" target="_blank" class="nav-link text-light">File an issue</a>
     </nav>
 </template>
