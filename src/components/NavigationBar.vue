44c6d080d7daa29bef5156df01cb2fd83a9bb62f
Jorrit Salverda <jsalverda@travix.com>
2018-06-21 16:40:50 +0200

    add link to estafette.io documentation

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -5,11 +5,14 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
+        <div class="navbar-nav mr-auto">
             <router-link :to="{ name: 'Pipelines'}" class="nav-item nav-link">Pipelines</router-link>
             <router-link :to="{ name: 'Targets'}" class="nav-item nav-link disabled">Targets</router-link>
             <router-link :to="{ name: 'Statistics'}" class="nav-item nav-link disabled">Statistics</router-link>
         </div>
+        <span class="navbar-text">
+            <a href="https://estafette.io/documentation/" target="_blank" class="nav-item nav-link">Documentation</a>
+        </span>
     </div>
     </nav>
 </template>
