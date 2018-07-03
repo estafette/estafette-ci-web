d79ea903eb199c0806a79aa30fdf8a7971ea0bc0
Jorrit Salverda <jsalverda@travix.com>
2018-07-03 16:11:34 +0200

    update footer links for renamed repo

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="footer nav justify-content-end bg-dark p-2">
         <a href="https://estafette.io/documentation/" target="_blank" class="nav-link text-light">Documentation</a>
-        <a href="https://github.com/estafette/estafette-ci" target="_blank" class="nav-link text-light">Github</a>
-        <a href="https://github.com/estafette/estafette-ci/issues/new" target="_blank" class="nav-link text-light">File an issue</a>
+        <a href="https://github.com/estafette/estafette-ci-central" target="_blank" class="nav-link text-light">Github</a>
+        <a href="https://github.com/estafette/estafette-ci-central/issues/new" target="_blank" class="nav-link text-light">File an issue</a>
     </nav>
 </template>
