b08549f01aa9e569684594266137eaa45769ad65
Jorrit Salverda <jorrit.salverda@gmail.com>
2018-07-06 17:09:58 +0200

    animate stats count with tweenlite

--- src/components/StatsBuildsCount.vue
+++ src/components/StatsBuildsCount.vue
@@ -2,13 +2,14 @@
     <div class="col-12 col-sm p-0 stat">
       <div class="rounded border border-success text-center">
         <h6 class="text-muted">Nr of builds</h6>
-        <p class="display-1">{{count}}</p>
+        <p class="display-1">{{animatedCount}}</p>
       </div>
     </div>
 </template>
 
 <script>
 import axios from 'axios'
+import TweenLite from 'gsap/TweenMax'
 
 export default {
   props: {
@@ -17,7 +18,7 @@ export default {
 
   data: function () {
     return {
-      count: '-'
+      count: 0
     }
   },
 
@@ -29,7 +30,7 @@ export default {
     loadStat () {
       axios.get(`/api/stats/buildscount?filter[since]=${this.filter.since}`)
         .then(response => {
-          this.count = response.data.count
+          TweenLite.to(this.$data, 0.5, { count: response.data.count })
           this.periodicallyRefreshStat(15)
         })
         .catch(e => {
@@ -48,6 +49,12 @@ export default {
       var timeoutWithJitter = Math.floor(Math.random() * (max - min + 1) + min)
 
       this.refreshTimeout = setTimeout(this.loadStat, timeoutWithJitter)
+    }
+  },
+
+  computed: {
+    animatedCount: function () {
+      return this.count.toFixed(0)
     }
   },
 
