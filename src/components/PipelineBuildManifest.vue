54ee3ca4522d9cea1d9baae181bd4577041211ee
Jorrit Salverda <jorrit.salverda@gmail.com>
2018-08-09 11:35:32 +0200

    add checkbox to show manifest with defaults

--- src/components/PipelineBuildManifest.vue
+++ src/components/PipelineBuildManifest.vue
@@ -1,11 +1,22 @@
 <template>
-  <pre class="bg-light m-3 p-3">{{build.manifest}}</pre>
+  <div>
+    <b-form-checkbox v-model="showDefaults" class="m-3">
+      Show defaults
+    </b-form-checkbox>
+    <pre v-if="showDefaults" class="bg-light m-3 p-3">{{build.manifestWithDefaults}}</pre>
+    <pre v-else class="bg-light m-3 p-3">{{build.manifest}}</pre>
+  </div>
 </template>
 
 <script>
 export default {
   props: {
     build: Object
+  },
+  data: function () {
+    return {
+      showDefaults: false
+    }
   }
 }
 </script>
