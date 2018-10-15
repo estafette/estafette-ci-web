e7a1d855845e4fc7aa8e1ddafbe1764de0c551b9
Jorrit Salverda <jorrit.salverda@gmail.com>
2018-10-15 14:45:11 +0200

    use eventsource for server sent events

--- src/components/PipelineBuildLogs.vue
+++ src/components/PipelineBuildLogs.vue
@@ -177,46 +177,38 @@ export default {
 
     tailLogs () {
       if (this.build.buildStatus === 'running') {
-        this.axios({
-          method: 'get',
-          url: `/api/pipelines/${this.repoSource}/${this.repoOwner}/${this.repoName}/builds/${this.id}/logs/tail`,
-          responseType: 'stream'
-        })
-          .then(response => {
-            var stream = response.data
-            stream.on('data', function (chunk) {
-              // event:log
-              // data:{"step":"git-clone","logLine":{"timestamp":"2018-10-15T11:40:04.105871286Z","streamType":"stderr","text":" Cloning into '/***-work'...\n"}}
-              console.log(chunk)
+        let es = new EventSource(`/api/pipelines/${this.repoSource}/${this.repoOwner}/${this.repoName}/builds/${this.id}/logs/tail`)
 
-              if (!this.log) {
-                this.log = {}
-              }
-              if (!this.log.steps) {
-                this.log.steps = []
-              }
+        es.addEventListener('log', event => {
+          let data = JSON.parse(event.data)
 
-              var step = this.log.steps.find(s => s.step === chunk.step)
-              if (!step) {
-                // set all previous steps to SUCCEEDED so they close
-                this.log.steps.forEach(s => {
-                  s.status = 'SUCCEEDED'
-                })
+          if (!this.log) {
+            this.log = {}
+          }
+          if (!this.log.steps) {
+            this.log.steps = []
+          }
 
-                // create new step
-                step = {step: chunk.step, logLines: [], exitCode: 0, status: 'RUNNING', autoInjected: false}
-                this.log.steps.push(step)
-              }
-
-              step.logLines.push(chunk.logLine)
+          var step = this.log.steps.find(s => s.step === data.step)
+          if (!step) {
+            // set all previous steps to SUCCEEDED so they close
+            this.log.steps.forEach(s => {
+              s.status = 'SUCCEEDED'
             })
-            stream.on('end', function () {
-              console.log('done streaming logs')
-            })
-          })
-          .catch(e => {
-            this.errors.push(e)
-          })
+
+            // create new step
+            step = {step: data.step, logLines: [], exitCode: 0, status: 'RUNNING', autoInjected: false}
+            this.log.steps.push(step)
+          }
+
+          step.logLines.push(data.logLine)
+        }, false)
+
+        es.addEventListener('error', event => {
+          if (event.readyState === EventSource.CLOSED) {
+            console.log('done streaming logs')
+          }
+        }, false)
       }
     }
   },
