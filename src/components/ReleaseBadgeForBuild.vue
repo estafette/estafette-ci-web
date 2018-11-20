7c0fc6fc47df51d61b7a1db28d259ca8f5df1d75
Jorrit Salverda <jsalverda@travix.com>
2018-11-20 12:06:32 +0100

    ensure v-for and v-if are not used on same element

--- src/components/ReleaseBadgeForBuild.vue
+++ src/components/ReleaseBadgeForBuild.vue
@@ -1,6 +1,6 @@
 <template>
   <span>
-      <router-link v-for="release in releaseTarget.activeReleases" v-bind:key="release.name" v-if="release && release.releaseVersion === build.buildVersion" :to="{ name: 'PipelineReleaseLogs', params: { repoSource: release.repoSource, repoOwner: release.repoOwner, repoName: release.repoName, releaseID: release.id }}" exact class="btn btn-light btn-sm mr-1">
+      <router-link v-for="release in matchingReleases" v-bind:key="release.name" :to="{ name: 'PipelineReleaseLogs', params: { repoSource: release.repoSource, repoOwner: release.repoOwner, repoName: release.repoName, releaseID: release.id }}" exact class="btn btn-light btn-sm mr-1">
         {{release.name}}<span class="badge ml-1" :class="release.releaseStatus | bootstrapClass('badge')">{{release.releaseStatus | defaultValue('-')}}</span>
       </router-link>
   </span>
@@ -11,6 +11,12 @@ export default {
   props: {
     releaseTarget: Object,
     build: Object
+  },
+
+  computed: {
+    matchingReleases: function () {
+      return this.releaseTarget.activeReleases.filter(r => r.releaseVersion === this.build.buildVersion)
+    }
   }
 }
 </script>
