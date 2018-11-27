980a91e05c6aae85b6894dddc0b5aead3cf507e6
Jorrit Salverda <jsalverda@travix.com>
2018-11-27 17:23:34 +0100

    filter out outliers when a timeline has more than 10 points

--- src/components/PipelineStatsTimeline.vue
+++ src/components/PipelineStatsTimeline.vue
@@ -159,6 +159,7 @@ export default {
     },
 
     updateSeries (durations) {
+      // filter outliers
       var durationsMap = {}
       durations.forEach(duration => {
         // generate key
@@ -188,8 +189,44 @@ export default {
           }
           this.series.push(serie)
         }
-        serie.data = durationsMap[key]
+
+        serie.data = this.filterOutliers(durationsMap[key])
       }
+    },
+
+    filterOutliers (durations) {
+      if (durations.length < 10) {
+        return durations
+      }
+
+      // copy the values, rather than operating on references to existing values
+      var values = durations.concat()
+
+      // then sort by duration
+      values.sort(function (a, b) {
+        return a.y > b.y
+      })
+
+      /* Then find a generous IQR. This is generous because if (values.length / 4)
+      * is not an int, then really you should average the two elements on either
+      * side to find q1.
+      */
+      var q1 = values[Math.floor((values.length / 4))].y
+      // Likewise for q3.
+      var q3 = values[Math.ceil((values.length * (3 / 4)))].y
+      var iqr = q3 - q1
+
+      // Then find min and max values
+      var maxValue = q3 + iqr * 1.5
+      var minValue = q1 - iqr * 1.5
+
+      // Then filter anything beyond or beneath these values.
+      var filteredValues = values.filter(function (x) {
+        return (x.y <= maxValue) && (x.y >= minValue)
+      })
+
+      // Then return
+      return filteredValues
     }
   },
 
