897ae5de39b3f3c79ed6542967f1fc3a4ae2418f
Jorrit Salverda <jsalverda@travix.com>
2018-11-29 15:19:41 +0100

    show manifest menu item; disable validate tab

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -7,7 +7,7 @@
         <b-collapse is-nav id="nav_collapse">
             <b-navbar-nav>
                 <b-nav-item :to="{ name: 'Pipelines'}">Pipelines</b-nav-item>
-                <!-- <b-nav-item :to="{ name: 'Manifest'}">Manifest</b-nav-item> -->
+                <b-nav-item :to="{ name: 'Manifest'}">Manifest</b-nav-item>
                 <b-nav-item :to="{ name: 'Statistics'}">Statistics</b-nav-item>
                 <b-nav-item v-if="user && user.authenticated" :to="{ name: 'Config'}">Configuration</b-nav-item>
             </b-navbar-nav>
