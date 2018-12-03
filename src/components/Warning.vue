02c7a4460e4eae7cdff195f561c93b567280ba49
Jorrit Salverda <jsalverda@travix.com>
2018-12-03 14:28:50 +0100

    allow markdown for warnings

--- src/components/Warning.vue
+++ src/components/Warning.vue
@@ -1,11 +1,16 @@
 <template>
 <div class="alert rounded border" :class="[alertClass, borderClass]" role="alert">
-  {{warning.message}}
+  <vue-markdown>{{warning.message}}</vue-markdown>
 </div>
 </template>
 
 <script>
+import VueMarkdown from 'vue-markdown'
+
 export default {
+  components: {
+    'vue-markdown': VueMarkdown
+  },
   props: {
     warning: Object
   },
