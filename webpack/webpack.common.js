9f1ef5d18ba991bfb3b3df5d41897b70c3257578
Jorrit Salverda <jsalverda@travix.com>
2018-12-17 16:29:25 +0100

    update fonts and images output names in rules

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -84,15 +84,18 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif)$/,
         include: path.resolve(__dirname, '..', 'src'),
-        use: [
-          'file-loader'
-        ]
+        loader: 'file-loader',
+        options: {
+          name: 'static/images/[name].[hash].[ext]'
+        }
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ]
+        include: path.resolve(__dirname, '..', 'src'),
+        loader: 'file-loader',
+        options: {
+          name: 'static/fonts/[name].[hash].[ext]'
+        }
       }
     ]
   }
