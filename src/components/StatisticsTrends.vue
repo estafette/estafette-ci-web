75cbfe5c3f228dc040137b2d5420bd33eefce2ee
Jorrit Salverda <jsalverda@travix.com>
2019-03-08 21:58:39 +0100

    fix spacing in trends page

--- src/components/StatisticsTrends.vue
+++ src/components/StatisticsTrends.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="m-3 row">
-    <stats-adoption
-      type="builds"
-      status="primary"
-    />
-    <stats-adoption
-      type="releases"
-      status="success"
-    />
+    <div class="col-12 col-xl-6">
+      <stats-adoption
+        type="builds"
+        status="primary"
+        class="mb-3"
+      />
+    </div>
+    <div class="col-12 col-xl-6">
+      <stats-adoption
+        type="releases"
+        status="success"
+        class="mb-3"
+      />
+    </div>
   </div>
 </template>
 
