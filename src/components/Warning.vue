06a9eb62aa953ce63fd536ac9ca69a457bb6c63b
Jorrit Salverda <jsalverda@travix.com>
2019-03-08 21:49:11 +0100

    icon for warning

--- src/components/Warning.vue
+++ src/components/Warning.vue
@@ -1,17 +1,28 @@
 <template>
   <div
-    class="alert rounded border"
+    class="alert rounded border d-flex flex-row h-100"
     :class="[alertClass, borderClass]"
     role="alert"
   >
+    <font-awesome-icon
+      icon="radiation"
+      class="h2 mr-3 align-self-center"
+    />
     <mark-down>{{ warning.message }}</mark-down>
   </div>
 </template>
 
 <script>
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faRadiation } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faRadiation)
+
 export default {
   components: {
-    'MarkDown': () => import(/* webpackChunkName: "marked" */ '@/components/MarkDown')
+    'MarkDown': () => import(/* webpackChunkName: "marked" */ '@/components/MarkDown'),
+    FontAwesomeIcon
   },
   props: {
     warning: {
