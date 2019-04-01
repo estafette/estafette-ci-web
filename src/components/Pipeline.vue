2f5c2ddbb378f49ea9f653cc1f6eb99b2f87378a
Jorrit Salverda <jsalverda@travix.com>
2019-04-01 17:10:03 +0200

    link labels to pipelines page

--- src/components/Pipeline.vue
+++ src/components/Pipeline.vue
@@ -118,7 +118,7 @@
         Labels
       </div>
       <router-link
-        :to="{ query: { status: filter && filter.status ? filter.status : '', since: filter && filter.since ? filter.since : '', labels: label.key + '=' + label.value, page: 1 } }"
+        :to="{ name: 'Pipelines', query: { status: filter && filter.status ? filter.status : '', since: filter && filter.since ? filter.since : '', labels: label.key + '=' + label.value, page: 1 } }"
         exact
         class="btn btn-light btn-sm mr-1"
         v-for="label in sortLabels(pipeline.labels)"
