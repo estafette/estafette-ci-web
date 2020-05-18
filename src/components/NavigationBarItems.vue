e038724de77ab668ed5659e72c1c1cbe3e29e06d
Jorrit Salverda <jsalverda@travix.com>
2020-05-18 10:42:24 +0200

    fix linting issue

--- src/components/NavigationBarItems.vue
+++ src/components/NavigationBarItems.vue
@@ -1,18 +1,18 @@
 <template>
-    <b-navbar-nav>
-        <b-nav-item
-            v-for="item in items"
-            :key="item.text"
-            :to="item.to"
-            class="mr-3 text-nowrap"
-        >
-            <font-awesome-icon
-            :icon="item.icon"
-            class="mr-2 d-none d-lg-inline"
-            />
-            {{item.text}}
-        </b-nav-item>
-    </b-navbar-nav>
+  <b-navbar-nav>
+    <b-nav-item
+      v-for="item in items"
+      :key="item.text"
+      :to="item.to"
+      class="mr-3 text-nowrap"
+    >
+      <font-awesome-icon
+        :icon="item.icon"
+        class="mr-2 d-none d-lg-inline"
+      />
+      {{ item.text }}
+    </b-nav-item>
+  </b-navbar-nav>
 </template>
 
 <script>
