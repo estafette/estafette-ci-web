39face409a53672cab2a2faba50a0aa32d05fbc2
Jorrit Salverda <jorrit.salverda@gmail.com>
2020-06-15 13:51:51 +0000

    estafette-ci-demo 0.0.40

--- mocks/api/pipelines/github.com/estafette/estafette-ci-demo/builds/564128788639285258/logs.stream/GET.js
+++
@@ -1,4 +0,0 @@
-const { delay, type, file } = require('connect-api-mocker/helpers')
-const path = require('path')
-
-module.exports = [delay(500), type('text/event-stream'), file(path.join(__dirname, './index.json'))]
