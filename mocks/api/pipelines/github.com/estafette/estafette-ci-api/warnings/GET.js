66a1a874819d08339caccccbe3fb72d7ac0404a6
Jorrit Salverda <jorrit.salverda@gmail.com>
2020-06-15 13:08:54 +0000

    estafette-ci-demo 0.0.34

--- mocks/api/pipelines/github.com/estafette/estafette-ci-api/warnings/GET.js
+++ mocks/api/pipelines/github.com/estafette/estafette-ci-api/warnings/GET.js
@@ -1,4 +1,4 @@
 const { delay, type, file } = require('connect-api-mocker/helpers')
 const path = require('path')
 
-module.exports = [delay(100), type('application/json'), file(path.join(__dirname, './index.json'))]
+module.exports = [delay(500), type('application/json'), file(path.join(__dirname, './index.json'))]
--- mocks/api/pipelines/github.com/estafette/estafette-ci-demo/builds/563921325648216074/logs/GET.js
+++
@@ -1,4 +0,0 @@
-const { delay, type, file } = require('connect-api-mocker/helpers')
-const path = require('path')
-
-module.exports = [delay(100), type('application/json'), file(path.join(__dirname, './index.json'))]
