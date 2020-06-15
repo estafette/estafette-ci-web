02bc75400797c749c834e0ee692c733b613e85db
Jorrit Salverda <jsalverda@travix.com>
2020-06-15 15:53:33 +0200

    update log stream file

--- docs/api/me/GET.js
+++ docs/api/me/GET.js
@@ -1,9 +1,10 @@
 const loggedIn = true
+const path = require('path')
 
 module.exports = function (request, response) {
   if (!loggedIn) {
     response.status(401).send({})
   } else {
-    response.sendFile('GET.json', { root: __dirname })
+    response.sendFile(path.join(__dirname, './index.json'))
   }
 }
