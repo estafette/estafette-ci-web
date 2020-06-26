9b0789a804aa356cc8f9f393abeef46089add78c
Jorrit Salverda <jsalverda@travix.com>
2020-06-26 04:48:17 +0200

    fix progress bar value calculation

--- src/filters/progressBarValue.js
+++ src/filters/progressBarValue.js
@@ -1,12 +1,12 @@
 export default function (pipeline, build, now) {
-  if (!pipeline || !pipeline.extraInfo || !build || !build.status) {
+  if (!pipeline || !pipeline.extraInfo || !build || !build.buildStatus) {
     return 100
   }
-  var status = build.status.toString().toLowerCase()
+  var status = build.buildStatus.toString().toLowerCase()
   if (status === 'pending') {
-    if (pipeline.extraInfo.medianPendingDuration > 0 && build.startedAt > 0) {
-      var pendingDuration = Math.pow(10, 6) * (now - build.startedAt)
-      var pendingValue = Math.round(100 * pendingDuration / pipeline.extraInfo.medianPendingDuration)
+    if (pipeline.extraInfo.medianPendingDuration > 0 && build.startedAt) {
+      var pendingDuration = now - new Date(build.startedAt)
+      var pendingValue = Math.round(100 * pendingDuration / pipeline.extraInfo.medianPendingDuration / Math.pow(10, 6))
       if (pendingValue >= 0 && pendingValue <= 100) {
         return pendingValue
       }
@@ -14,12 +14,12 @@ export default function (pipeline, build, now) {
     return 100
   }
   if (status === 'running') {
-    if (pipeline.extraInfo.medianDuration > 0 && build.startedAt > 0) {
-      var duration = Math.pow(10, 6) * (now - build.startedAt)
+    if (pipeline.extraInfo.medianDuration > 0 && build.startedAt) {
+      var duration = now - new Date(build.startedAt)
       if (build.pendingDuration > 0) {
-        duration -= build.pendingDuration
+        duration -= build.pendingDuration / Math.pow(10, 6)
       }
-      var value = Math.round(100 * duration / pipeline.extraInfo.medianDuration)
+      var value = Math.round(100 * duration / pipeline.extraInfo.medianDuration / Math.pow(10, 6))
       if (value >= 0 && value <= 100) {
         return value
       }
