b36567b1fa45c932fa5577dc54226f2de4ad5f77
Jorrit Salverda <jsalverda@travix.com>
2020-06-26 04:29:42 +0200

    convert milliseconds to nanoseconds for progress calculation

--- src/filters/progressBarValue.js
+++ src/filters/progressBarValue.js
@@ -5,7 +5,7 @@ export default function (pipeline, build, now) {
   var status = build.status.toString().toLowerCase()
   if (status === 'pending') {
     if (pipeline.extraInfo.medianPendingDuration > 0 && build.startedAt > 0) {
-      var pendingDuration = now - build.startedAt
+      var pendingDuration = Math.pow(10, 6) * (now - build.startedAt)
       var pendingValue = Math.round(100 * pendingDuration / pipeline.extraInfo.medianPendingDuration)
       if (pendingValue >= 0 && pendingValue <= 100) {
         return pendingValue
@@ -15,7 +15,7 @@ export default function (pipeline, build, now) {
   }
   if (status === 'running') {
     if (pipeline.extraInfo.medianDuration > 0 && build.startedAt > 0) {
-      var duration = now - build.startedAt
+      var duration = Math.pow(10, 6) * (now - build.startedAt)
       if (build.pendingDuration > 0) {
         duration -= build.pendingDuration
       }
