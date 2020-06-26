2106c9298bb282d50376758bf69c4be994f71cd3
Jorrit Salverda <jsalverda@travix.com>
2020-06-26 04:59:29 +0200

    convert median duration to ms first

--- src/filters/progressBarValue.js
+++ src/filters/progressBarValue.js
@@ -5,8 +5,9 @@ export default function (pipeline, build, now) {
   var status = build.buildStatus.toString().toLowerCase()
   if (status === 'pending') {
     if (pipeline.extraInfo.medianPendingDuration > 0 && build.insertedAt) {
-      var pendingDuration = now - new Date(build.insertedAt)
-      var pendingValue = Math.round(100 * pendingDuration / pipeline.extraInfo.medianPendingDuration / Math.pow(10, 6))
+      var pendingDurationMs = now - new Date(build.insertedAt)
+      var medianPendingDurationMs = pipeline.extraInfo.medianPendingDuration / Math.pow(10, 6)
+      var pendingValue = Math.round(100 * pendingDurationMs / medianPendingDurationMs)
       if (pendingValue >= 0 && pendingValue <= 100) {
         return pendingValue
       }
@@ -15,8 +16,9 @@ export default function (pipeline, build, now) {
   }
   if (status === 'running') {
     if (pipeline.extraInfo.medianDuration > 0 && build.startedAt) {
-      var duration = now - new Date(build.startedAt)
-      var value = Math.round(100 * duration / pipeline.extraInfo.medianDuration / Math.pow(10, 6))
+      var durationMs = now - new Date(build.startedAt)
+      var medianDurationMs = pipeline.extraInfo.medianDuration / Math.pow(10, 6)
+      var value = Math.round(100 * durationMs / medianDurationMs)
       if (value >= 0 && value <= 100) {
         return value
       }
