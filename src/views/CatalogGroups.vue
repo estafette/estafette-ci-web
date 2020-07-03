fc8db53c4e8fad8011f283f05a0abec6b68fdeb6
Jorrit Salverda <jsalverda@travix.com>
2020-07-03 16:05:46 +0200

    limit number of columns for catalog groups page

--- src/views/CatalogGroups.vue
+++ src/views/CatalogGroups.vue
@@ -1,31 +1,38 @@
 <template>
-  <b-card-group deck>
-    <b-card
+  <b-row>
+    <b-col
       v-for="group in groups"
       :key="group.id"
-      :title="group.name"
-      tag="article"
+      cols="12"
+      lg="4"
+      class="mb-3 col-xxxl-3"
     >
-      <b-card-text>
-        {{ group.description }}
-      </b-card-text>
-
-      <b-button
-        :to="{ name: 'CatalogGroupDetails', params: { id: group.id } }"
-        variant="primary"
+      <b-card
+        :title="group.name"
+        tag="article"
       >
-        Details
-      </b-button>
-    </b-card>
-  </b-card-group>
+        <b-card-text>
+          {{ group.description }}
+        </b-card-text>
+
+        <b-button
+          :to="{ name: 'CatalogGroupDetails', params: { id: group.id } }"
+          variant="primary"
+        >
+          Details
+        </b-button>
+      </b-card>
+    </b-col>
+  </b-row>
 </template>
 
 <script>
-import { BCardGroup, BCard, BCardText, BButton } from 'bootstrap-vue'
+import { BRow, BCol, BCard, BCardText, BButton } from 'bootstrap-vue'
 
 export default {
   components: {
-    BCardGroup,
+    BRow,
+    BCol,
     BCard,
     BCardText,
     BButton
