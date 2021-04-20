6de496ce0793d015f2de2f857f16dc19ef8e210c
Jorrit Salverda <jorrit.salverda@gmail.com>
2021-04-20 09:47:07 +0000

    estafette-ci-demo 0.0.43

--- mocks/api/pipelines/github.com/estafette/estafette-letsencrypt-certificate/builds/592931953286512650/warnings/GET.js
+++
@@ -1,4 +0,0 @@
-const { delay, type, file } = require('connect-api-mocker/helpers')
-const path = require('path')
-
-module.exports = [delay(500), type('application/json'), file(path.join(__dirname, './index.json'))]
